191d2ff0bd2220002473c1a4006d0d0b46903683
Simon <63975668+Simyon264@users.noreply.github.com>
2024-04-09 17:27:56 +0200

    Add DataController.cs
    Currently only contains username completion based on stored name data.

---
+++ Server/Api/DataController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Server.Api;
+
+/// <summary>
+/// Contains endpoints for data retrieval. Such as search completions, leaderboards, and more.
+/// </summary>
+[ApiController]
+[EnableCors]
+[Route("api/[controller]")]
+public class DataController : ControllerBase
+{
+    private readonly ReplayDbContext _context;
+
+    public DataController(ReplayDbContext context)
+    {
+        _context = context;
+    }
+
+    /// <summary>
+    /// Provides a list of usernames which start with the given username.
+    /// </summary>
+    /// <param name="username"></param>
+    /// <returns></returns>
+    [HttpGet]
+    [Route("username-completion")]
+    public async Task<ActionResult> GetUsernameCompletion(
+        [FromQuery] string username
+    )
+    {
+        var completions = await _context.Players
+            .Where(p => p.PlayerOocName.ToLower().StartsWith(username.ToLower()))
+            .Select(p => p.PlayerOocName)
+            .Distinct() // Remove duplicates
+            .Take(10)
+            .ToListAsync();
+
+        return Ok(completions);
+    }
+}
