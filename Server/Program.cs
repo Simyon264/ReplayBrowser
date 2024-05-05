9457ff04ce451516f1234782e0e36a638087fe46
Simon <63975668+Simyon264@users.noreply.github.com>
2024-05-05 16:17:32 +0200

    Revert WithTracing and EntityFrameworkCoreInstrumentation

--- Server/Program.cs
+++ Server/Program.cs
@@ -141,11 +141,6 @@ try
                     0, 100, 200, 300, 400, 500
                 }
             });
-    }).WithTracing(providerBuilder =>
-    {
-        providerBuilder.AddHttpClientInstrumentation();
-        providerBuilder.AddAspNetCoreInstrumentation();
-        providerBuilder.AddEntityFrameworkCoreInstrumentation();
     });
     
     var app = builder.Build();
