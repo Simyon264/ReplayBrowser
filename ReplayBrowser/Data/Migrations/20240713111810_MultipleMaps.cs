806c277c164be3069a34e598a118d97c88596af9
Simon <63975668+Simyon264@users.noreply.github.com>
2024-07-13 13:18:47 +0200

    whoops, forgot to add this

---
+++ ReplayBrowser/Data/Migrations/20240713111810_MultipleMaps.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class MultipleMaps : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Map",
+                table: "Replays",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<List<string>>(
+                name: "Maps",
+                table: "Replays",
+                type: "text[]",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Maps",
+                table: "Replays");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Map",
+                table: "Replays",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
