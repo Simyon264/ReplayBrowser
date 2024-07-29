16cec8f55e7fd5bf93f8adaa152df19b5ffe7fc5
Simon <63975668+Simyon264@users.noreply.github.com>
2024-07-29 16:29:56 +0200

    Migration for protected accounts

---
+++ ReplayBrowser/Data/Migrations/20240729142846_ProtectedAccounts.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProtectedAccounts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Protected",
+                table: "Accounts",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Protected",
+                table: "Accounts");
+        }
+    }
+}
