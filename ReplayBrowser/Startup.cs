f5018f6a95b8fa45ecdec6da59858b30521ee3e0
Saphire <lattice@saphi.re>
2024-08-20 23:18:41 +0600

    Remove profile pregen

--- ReplayBrowser/Startup.cs
+++ ReplayBrowser/Startup.cs
@@ -24,16 +24,16 @@ public class Startup
     {
         Configuration = configuration;
     }
-    
+
     public IConfiguration Configuration { get; }
-    
+
     public void ConfigureServices(IServiceCollection services)
     {
         services.AddMemoryCache(opts =>
         {
             opts.TrackStatistics = true;
         });
-        
+
         services.AddControllersWithViews().AddJsonOptions(options =>
         {
             options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
@@ -52,11 +52,11 @@ public class Startup
             options.EnableSensitiveDataLogging();
             options.UseLoggerFactory(LoggerFactory.Create(builder => builder.AddConsole().AddSerilog()));
         });
-        
+
         // Run migrations on startup.
         var replayContext = services.BuildServiceProvider().GetService<ReplayDbContext>();
         replayContext.Database.Migrate();
-        
+
         // We need to create a "dummy" system Account to use for unauthenticated requests.
         // This is because the Account system is used for logging and we need to have an account to log as.
         // This is a bit of a hack but it works.
@@ -67,7 +67,7 @@ public class Startup
                 Guid = Guid.Empty,
                 Username = "[System] Unauthenticated user",
             };
-        
+
             if (replayContext.Accounts.FirstOrDefault(a => a.Guid == Guid.Empty) == null) // Only add if it doesn't already exist.
             {
                 replayContext.Accounts.Add(systemAccount);
@@ -78,23 +78,21 @@ public class Startup
         {
             Log.Error(e, "Failed to create system account.");
         }
-        
+
         services.AddSingleton<Ss14ApiHelper>();
         services.AddSingleton<AccountService>();
         services.AddSingleton<LeaderboardService>();
         services.AddSingleton<ReplayParserService>();
         services.AddSingleton<AnalyticsService>();
         services.AddSingleton<NoticeHelper>();
-        services.AddSingleton<ProfilePregeneratorService>();
-        
+
         services.AddHostedService<BackgroundServiceStarter<ReplayParserService>>();
         services.AddHostedService<BackgroundServiceStarter<AccountService>>();
         services.AddHostedService<BackgroundServiceStarter<LeaderboardService>>();
         services.AddHostedService<BackgroundServiceStarter<AnalyticsService>>();
-        services.AddHostedService<BackgroundServiceStarter<ProfilePregeneratorService>>();
-        
+
         services.AddScoped<ReplayHelper>();
-        
+
         services.AddCors(options =>
         {
             options.AddDefaultPolicy(builder =>
@@ -104,7 +102,7 @@ public class Startup
                     .AllowAnyHeader();
             });
         });
-        
+
         // Endpoint logging
         services.Configure<RequestLoggingOptions>(options =>
         {
@@ -120,7 +118,7 @@ public class Startup
                 Log.Fatal("No proxy IP found in appsettings.json. Exiting.");
                 Environment.Exit(1);
             }
-            
+
             Log.Information("Proxy IP: {ProxyIP}", proxyIP);
             options.KnownProxies.Add(IPAddress.Parse(proxyIP));
         });
@@ -128,23 +126,23 @@ public class Startup
         services.AddOpenTelemetry().WithMetrics(providerBuilder =>
         {
             providerBuilder.AddPrometheusExporter();
-    
-    
+
+
             providerBuilder.AddMeter("Microsoft.AspNetCore.Hosting",
                 "Microsoft.AspNetCore.Server.Kestrel");
-    
+
             providerBuilder.AddAspNetCoreInstrumentation()
                 .AddHttpClientInstrumentation()
                 .AddRuntimeInstrumentation()
                 .AddProcessInstrumentation();
-    
+
             providerBuilder.AddMeter(
                 "Microsoft.Extensions.Diagnostics.ResourceMonitoring",
                 "Microsoft.AspNetCore.Routing",
                 "Microsoft.AspNetCore.Diagnostics",
                 "System.Net.Http",
                 "ReplayBrowser");
-    
+
             providerBuilder.AddView("http.server.request.duration",
                 new ExplicitBucketHistogramConfiguration
                 {
@@ -154,7 +152,7 @@ public class Startup
                         0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10
                     }
                 });
-    
+
             providerBuilder.AddView("http.server.request.size",
                 new ExplicitBucketHistogramConfiguration
                 {
@@ -163,7 +161,7 @@ public class Startup
                         0, 100, 1024, 1024 * 10, 1024 * 100, 1024 * 1024, 1024 * 1024 * 10, 1024 * 1024 * 100
                     }
                 });
-    
+
             providerBuilder.AddView("http.server.response.size",
                 new ExplicitBucketHistogramConfiguration
                 {
@@ -172,7 +170,7 @@ public class Startup
                         0, 100, 1024, 1024 * 10, 1024 * 100, 1024 * 1024, 1024 * 1024 * 10, 1024 * 1024 * 100
                     }
                 });
-    
+
             providerBuilder.AddView("http.server.response.duration",
                 new ExplicitBucketHistogramConfiguration
                 {
@@ -182,7 +180,7 @@ public class Startup
                         0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10
                     }
                 });
-    
+
             providerBuilder.AddView("http.server.response.status",
                 new ExplicitBucketHistogramConfiguration
                 {
@@ -197,7 +195,7 @@ public class Startup
             .AddInteractiveServerComponents();
 
         services.AddHttpContextAccessor();
-        
+
         JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
 
         services.AddAuthentication(options =>
@@ -210,25 +208,25 @@ public class Startup
         }).AddOpenIdConnect("oidc", options =>
         {
             options.SignInScheme = "Cookies";
-    
+
             options.Authority = "https://central.spacestation14.io/web/";
             options.ClientId = Configuration["ClientId"];
             options.ClientSecret = Configuration["ClientSecret"];
-    
+
             options.ResponseType = OpenIdConnectResponseType.Code;
             options.RequireHttpsMetadata = false;
-    
+
             options.Scope.Add("openid");
             options.Scope.Add("profile");
-    
+
             options.GetClaimsFromUserInfoEndpoint = true;
         });
-        
+
         services.AddHttpLogging(o => { });
-        
+
         services.AddSwaggerGen();
     }
-    
+
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
         app.Use((context, next) =>
@@ -236,7 +234,7 @@ public class Startup
             context.Request.Scheme = "https";
             return next();
         });
-        
+
         if (!env.IsDevelopment())
         {
             app.UseExceptionHandler("/Error", createScopeForErrors: true);
@@ -246,20 +244,20 @@ public class Startup
         {
             app.UseDeveloperExceptionPage();
         }
-        
+
         app.UseHttpsRedirection();
-        
+
         app.UseStaticFiles();
 
         app.UseRouting();
-        
+
         app.UseAuthentication();
         app.UseAuthorization();
         app.UseAntiforgery();
-        
+
         app.UseSwagger();
         app.UseSwaggerUI();
-        
+
         app.UseEndpoints(endpoints =>
         {
             endpoints.MapSwagger();
@@ -271,7 +269,7 @@ public class Startup
                 name: "default",
                 pattern: "api/{controller=Home}/{action=Index}/{id?}");
         });
-        
+
         app.UseHttpsRedirection();
         app.UseForwardedHeaders();
         app.UseRouting();
