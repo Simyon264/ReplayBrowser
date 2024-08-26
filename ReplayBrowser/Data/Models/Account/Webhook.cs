6975225e6c200d60fb77cacbada6d3fb79c8cee2
Simon <63975668+Simyon264@users.noreply.github.com>
2024-08-26 19:42:45 +0200

    Fix warning

--- ReplayBrowser/Data/Models/Account/Webhook.cs
+++ ReplayBrowser/Data/Models/Account/Webhook.cs
@@ -4,7 +4,7 @@ public class Webhook
 {
     public int Id { get; set; }
 
-    public string Url { get; set; }
+    public string Url { get; set; } = null!;
     public WebhookType Type { get; set; }
     public List<WebhookHistory> Logs { get; set; } = new();
 }
