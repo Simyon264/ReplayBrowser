742f398d5e4494db4b03ebb0fbb6c2e4cc711627
Simon <63975668+Simyon264@users.noreply.github.com>
2024-08-26 14:00:21 +0200

    Migration tests (#51)
    * Migration tests
    
    * Whoops
    
    * maybe python was a bad idea
    
    * Aha!
    
    * Syntax highlighting failed me
    
    * Unfuck DB
    
    * oh shit i forgot these
    
    * ah fuck im too tired for this
    
    * mhm logging
    
    * Page tests
    
    * forgot password
    
    * uhhh yeah
    
    * shit i forgot to actually run the tests
    
    * AHHHH
    
    * im gonna go crazy
    
    * NOW? PLEASE?
    
    * i just want this to work man
    
    * i will throw myself off a bridge
    
    * i mean it
    
    * oh my fucking god all this time it couldn't find the project to run
    
    i want to kms
    
    * MAYBE??????????????
    
    * why did I design it this way what
    
    * OK I FEEL IT
    
    * ok last one im going to sleep now
    
    * this time fr fr
    
    * Testing how an error looks
    
    * errr
    
    * Finalize testing
    
    * Fix warning?

---
+++ Tools/check_model_pending_changes.py
@@ -0,0 +1,30 @@
+﻿# This script checks if there are any pending changes in the model and if so, it will fail.
+# This is useful to prevent committing changes to the model that are not intended.
+
+import subprocess
+import sys
+
+def main():
+    try:
+        # Run the dotnet ef migrations has-pending-model-changes command
+        result = subprocess.run(
+            ['dotnet', 'ef', 'migrations', 'has-pending-model-changes', '--project', './ReplayBrowser/ReplayBrowser.csproj'],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+
+        # Check the output and return code
+        if result.returncode == 0:
+            print("No pending model changes detected.")
+        else:
+            print("Pending model changes detected:")
+            print(result.stdout)
+            sys.exit(1)
+
+    except Exception as e:
+        print(f"An error occurred while checking for pending migrations: {str(e)}")
+        sys.exit(1)
+
+if __name__ == '__main__':
+    main()
