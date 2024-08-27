dba9141db0220a7f7b1d0369fecb7bbc4e3338c9
Simon <63975668+Simyon264@users.noreply.github.com>
2024-08-27 03:28:56 +0200

    Server filtered webhooks (#53)

--- ReplayBrowser/Data/Models/Account/Webhook.cs
+++ ReplayBrowser/Data/Models/Account/Webhook.cs
@@ -7,4 +7,12 @@ public class Webhook
     public string Url { get; set; } = null!;
     public WebhookType Type { get; set; }
     public List<WebhookHistory> Logs { get; set; } = new();
+
+    /// <summary>
+    /// Comma seperated list of servers that this webhook is allowed to be called from.
+    /// </summary>
+    /// <remarks>
+    /// It's comma seperated because I cannot be bothered to make the UI for this. An ideal solution would be a list of strings.
+    /// </remarks>
+    public string Servers { get; set; } = null!;
 }
