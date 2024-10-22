28cb25df09b439b18c30c2802ab1d2271fe9e670
Simon <63975668+Simyon264@users.noreply.github.com>
2024-10-22 11:50:41 +0200

    i forgot to add the db migration files
    I HATE GIT I HATE GIT I HATE GIT

---
+++ ReplayBrowser/Data/Migrations/20241021102315_ServerTokens.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class ServerTokens : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ServerTokens",
+                columns: table => new
+                {
+                    Token = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ServerTokens", x => x.Token);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ServerTokens_Token",
+                table: "ServerTokens",
+                column: "Token");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ServerTokens");
+        }
+    }
+}
