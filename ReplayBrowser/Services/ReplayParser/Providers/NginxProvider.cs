3b2e53862a68c1054293f693ec770eeb29957321
Simon <63975668+Simyon264@users.noreply.github.com>
2024-10-29 11:45:48 +0100

    Remove spammy info log

--- ReplayBrowser/Services/ReplayParser/Providers/NginxProvider.cs
+++ ReplayBrowser/Services/ReplayParser/Providers/NginxProvider.cs
@@ -8,33 +8,32 @@ public class NginxProvider : ReplayProvider
 {
     public override async Task RetrieveFilesRecursive(string directoryUrl, CancellationToken token)
     {
-        Log.Information("Retrieving files from " + directoryUrl);
         var client = GetHttpClient();
         var htmlContent = await client.GetStringAsync(directoryUrl, token);
         var document = new HtmlDocument();
         document.LoadHtml(htmlContent);
-            
+
         var links = document.DocumentNode.SelectNodes("//a[@href]");
         if (links == null)
         {
-            Log.Information("No links found on " + directoryUrl + ".");
+            Log.Warning("No links found on " + directoryUrl + ".");
             return;
         }
-            
+
         foreach (var link in links)
         {
             if (token.IsCancellationRequested)
             {
                 return;
             }
-                
+
             var href = link.Attributes["href"].Value;
 
             if (href.StartsWith("..", StringComparison.Ordinal))
             {
                 continue;
             }
-                
+
             if (!Uri.TryCreate(href, UriKind.Absolute, out _))
             {
                 href = new Uri(new Uri(directoryUrl), href).ToString();
@@ -45,7 +44,7 @@ public class NginxProvider : ReplayProvider
                 await Task.Delay(TimeSpan.FromMilliseconds(10), token); // Prevents spamming the server
                 await RetrieveFilesRecursive(href, token);
             }
-                
+
             if (href.EndsWith(".zip", StringComparison.Ordinal))
             {
                 await ReplayParserService.AddReplayToQueue(href);
